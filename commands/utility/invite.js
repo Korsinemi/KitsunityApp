6dc5cff3fa6d70d1531fcac4e1f403995f054068
KitsuneCode <im.kitsunecode@gmail.com>
2021-05-25 14:28:08 -0500

    Reparando comandos

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -17,7 +17,7 @@ module.exports = {
     .setDescription('**[💖 Añademe dando click aqui!!](https://discord.com/oauth2/authorize?client_id=831865259357896755&permissions=8&scope=bot%20applications.commands)**\nAl invitarla a tu servidor me ayudas a seguir mejorando mi proyecto y seguir actualizandola UwU')
     .setColor('RANDOM')
     .setImage('https://kitsunityx.glitch.me/api/sfw/wag/wag1.gif')
-    .setFooter('Hecho con 💖 por KitsuneCode#5011, nyah nwn', creator.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+    .setFooter('Hecho con 💖 por KitsuneCode#5011, nyah nwn')
     return message.author.send(embed);
   }
 };
