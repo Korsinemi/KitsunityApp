179dbe9cfb90b7bc202a8f709be7cb21f961b140
KitsuneCode <im.kitsunecode@gmail.com>
2021-05-30 09:30:49 -0500

    Reparando comandos

--- commands/utility/info.js
+++ commands/utility/info.js
@@ -13,42 +13,43 @@ module.exports = {
   catergory: 'Utilidad',
   async execute(client, message, args) {
     function checkDays(date) {
-        let now = new Date();
-        let diff = now.getTime() - date.getTime();
-        let days = Math.floor(diff / 86400000);
-  
-        if (days >= 30){
-          return "Hace un mes | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 60){
-          return "Hace dos meses  | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 90){
-          return "Hace tres meses | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 120){
-          return "Hace cuatro meses | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 150){
-          return "Hace cinco meses | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 180){
-          return "Hace seis meses | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 210){
-          return "Hace siete meses | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 240){
-          return "Hace ocho meses | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 270){
-          return "Hace nueve meses | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 300){
-          return "Hace diez meses | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 330){
-          return "Hace once meses | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 365){
-          return "Hace un año | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 730){
-          return "Hace dos años | " + days + (days == 1 ? " dia" : " dias");
-        } else if (days >= 1095) {
-        return "Hace tres años o mas | " + days + (days == 1 ? " dia" : " dias");
-        } else {
-          return "Hace " + days + (days == 1 ? " dia" : " dias");
-        }
-      };
+      let now = new Date();
+      let diff = now.getTime() - date.getTime();
+      let days = Math.floor(diff / 86400000);
+
+      if (days >= 30 && days < 59){
+        return "Hace un mes | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 60 && days < 89){
+        return "Hace dos meses  | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 90 && days < 119){
+        return "Hace tres meses | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 120 && days < 149){
+        return "Hace cuatro meses | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 150 && days < 179){
+        return "Hace cinco meses | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 180 && days < 209){
+        return "Hace seis meses | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 210 && days < 239){
+        return "Hace siete meses | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 240 && days < 269){
+        return "Hace ocho meses | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 270 && days < 299){
+        return "Hace nueve meses | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 300 && days < 329){
+        return "Hace diez meses | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 330 && days < 364){
+        return "Hace ocho meses | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 365 && days < 729){
+        return "Hace un año | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 730 && days < 1094){
+        return "Hace dos años | " + days + (days == 1 ? " dia" : " dias");
+      } else if (days >= 1095) {
+      return "Hace tres años o mas | " + days + (days == 1 ? " dia" : " dias");
+      } else {
+        return "Hace " + days + (days == 1 ? " dia" : " dias");
+      }
+    };
+
     let ServerPrefix = await db.get(`${message.guild.id}_prefix`);
     const kitsunitydate = moment(client.user.createdAt).format("DD/MM/YYYY - h:mm A");
     const authoricon = client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 });
